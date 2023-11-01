fb9840ee5ab4121f085e1f4e058245b0a8084abf
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2023-11-01 23:24:48 +0100

    refactor

--- src/components/Step2.tsx
+++ src/components/Step2.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef, type ComponentPropsWithoutRef } from "react";
+import { twMerge } from "tailwind-merge";
 
 export type ModalProps = ComponentPropsWithoutRef<"dialog"> & {
   onClose: () => void;
 };
 
 export function Modal(props: ModalProps) {
-  const { children, open, onClose, ...rest } = props;
+  const { children, open, onClose, className, ...rest } = props;
   const ref = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
@@ -32,8 +33,12 @@ export function Modal(props: ModalProps) {
   }, [onClose]);
 
   return (
-    <dialog ref={ref} {...rest}>
-      {children}
+    <dialog ref={ref} className={twMerge("group", className)} {...rest}>
+      <div className="fixed inset-0 grid place-content-center bg-black/75 opacity-0 transition-all group-open:opacity-100">
+        <div className="w-full max-w-lg scale-75 bg-white p-4 opacity-0 shadow-lg transition-all group-open:scale-100 group-open:opacity-100">
+          {children}
+        </div>
+      </div>
     </dialog>
   );
 }
